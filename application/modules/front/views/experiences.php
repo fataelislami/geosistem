1afeffa9581d693ba832926a0b38322c3d7a4c19
Fata El Islami <fataelislami97@gmail.com>
2019-08-30 10:21:28 +0700

    Add li hidden

--- application/modules/front/views/experiences.php
+++ application/modules/front/views/experiences.php
@@ -26,74 +26,80 @@
             <div class="mt-post-text">
               <div class="row">
                 <div class="col-md-6">
-                  <ul class="list-angle-right anchor-line">
-                      <li>1. Nanggroe Aceh Darussalam Dam,Sanitary landﬁll, WWTP</li>
+                  <ol class="list-num-count list-outline">
+                  <li>Nanggroe Aceh Darussalam Dam,Sanitary landﬁll, WWTP</li>
 
-                      <li>2. Nort Sumatera Airport, Seaport, Sanitary Landﬁll</li>
+                  <li>Nort Sumatera Airport, Seaport, Sanitary Landﬁll</li>
 
-                      <li>3. Riau Seaport, Power Plant, Sanitary Landﬁll, Floating Cover</li>
+                  <li>Riau Seaport, Power Plant, Sanitary Landﬁll, Floating Cover</li>
 
-                      <li>4. Riau Countinental Power Plant, Sanitary Landﬁll</li>
+                  <li>Riau Countinental Power Plant, Sanitary Landﬁll</li>
 
-                      <li>5. West Sumatera Sanitary Landﬁll</li>
+                  <li>West Sumatera Sanitary Landﬁll</li>
 
-                      <li>6. Jambi Oil & gas</li>
+                  <li>Jambi Oil & gas</li>
 
-                      <li>7. Bangka Belitung Reservoir, Floating Cover</li>
+                  <li>Bangka Belitung Reservoir, Floating Cover</li>
 
-                      <li>8. South Sumatera Seaport, Sanitary Landﬁll, Toll Road</li>
+                  <li>South Sumatera Seaport, Sanitary Landﬁll, Toll Road</li>
 
-                      <li>9. Lampung Sanitary Landﬁll</li>
+                  <li>Lampung Sanitary Landﬁll</li>
 
-                      <li>10. Nort Kalimantan Airport, Reservoir, Mining, Sanitary Landﬁll, Seaport</li>
+                  <li>Nort Kalimantan Airport, Reservoir, Mining, Sanitary Landﬁll, Seaport</li>
 
-                      <li>11. East Kalimantan Highway, Airport, Seaport, Stadium, Real Estate, Industrial Estate,Reservoir, Oil & Gas, Sanitary Landﬁll WWTP, Mudpit, Toll Road</li>
+                  <li>East Kalimantan Highway, Airport, Seaport, Stadium, Real Estate, Industrial Estate,Reservoir, Oil & Gas, Sanitary Landﬁll WWTP, Mudpit, Toll Road</li>
 
-                      <li>12. Central Kalimantan Mining, Sanitary Landﬁll, WWTP</li>
+                  <li>Central Kalimantan Mining, Sanitary Landﬁll, WWTP</li>
 
-                      <li>13. South Kalimantan Seaport, Sanitary Landﬁll, Mudpit, Mining, Oil & Gas, Highway,</li>
+                  <li>South Kalimantan Seaport, Sanitary Landﬁll, Mudpit, Mining, Oil & Gas, Highway,</li>
 
-                      <li>14. Banten Power Plant, sanitary Landﬁll, Reservoir</li>
+                  <li>Banten Power Plant, sanitary Landﬁll, Reservoir</li>
 
-                      <li>15. Jakarta Seaport, Real Estate, Reclamation, Airport</li>
+                  <li>Jakarta Seaport, Real Estate, Reclamation, Airport</li>
 
-                      <li>16. West Java Toll Road, Stadium, Real Estate, Oil & Gas, Sanitary, Landﬁll, Reservoir</li>
-                      <li>17. Central Java Highway, Toll Road, Airport, Seaport, Power Plant Reservoir, Sanitary Landﬁll, WWTP</li>
+                  <li>West Java Toll Road, Stadium, Real Estate, Oil & Gas, Sanitary, Landﬁll, Reservoir</li>
 
-                      </ul>
+                  <li>PT. Teknindo Geosistem Unggul Wisma SIER, 1st Floor Rungkut Industri Raya No. 10 Surabaya 60293 Indonesia</li>
+
+                </ol>
                 </div>
                 <div class="col-md-6">
-                  <ul class="list-angle-right anchor-line">
+                  <ol class="list-num-count list-outline" id="colStartLi">
+                    <?php for($i=1;$i<=17;$i++){?>
+                      <li style="visibility:hidden;position:fixed"></li>
+                    <?php } ?>
 
-                    <li>18. Yogyakarta Reservoir, Sanitary Landﬁll, Aquaculture</li>
+                    <li>Central Java Highway, Toll Road, Airport, Seaport, Power Plant Reservoir, Sanitary Landﬁll, WWTP</li>
 
-                    <li>19. East Java Highway, Toll Road, Airport, Seaport, Stadium, Real Estate, Industrial Estate Reservoir, Power Plant, Oil & Gas, Sanitary Landﬁll, Floating Cover, Aquaculture, Mudpit</li>
+                    <li>Yogyakarta Reservoir, Sanitary Landﬁll, Aquaculture</li>
 
-                    <li>20. Bali Reservoir, Golf Course, Dam</li>
+                  <li>East Java Highway, Toll Road, Airport, Seaport, Stadium, Real Estate, Industrial Estate Reservoir, Power Plant, Oil & Gas, Sanitary Landﬁll, Floating Cover, Aquaculture, Mudpit</li>
 
-                    <li>21. West Nusa Tenggara Reservoir, Power Plant, Sanitary Landﬁll, Dam</li>
+                  <li>Bali Reservoir, Golf Course, Dam</li>
 
-                    <li>22. East Nusa Tenggara Reservoir, Power Plant, Sanitary Landﬁll</li>
+                  <li>West Nusa Tenggara Reservoir, Power Plant, Sanitary Landﬁll, Dam</li>
 
-                    <li>23. North Sulawesi Sanitary Landﬁll</li>
+                  <li>East Nusa Tenggara Reservoir, Power Plant, Sanitary Landﬁll</li>
 
-                    <li>24. Gorontalo Sanitary Landﬁll</li>
+                  <li>North Sulawesi Sanitary Landﬁll</li>
 
-                    <li>25. Central Sulawesi Oil & Gas, Sanitary Landﬁll, Slope Protection</li>
+                  <li>Gorontalo Sanitary Landﬁll</li>
 
-                    <li>26. West Sulawesi Sanitary Landﬁll, WWTP</li>
+                  <li>Central Sulawesi Oil & Gas, Sanitary Landﬁll, Slope Protection</li>
 
-                    <li>27. Southeast Sulawesi Sanitary Landﬁll, Seaport</li>
+                  <li>West Sulawesi Sanitary Landﬁll, WWTP</li>
 
-                    <li>28. South Sulawesi Seaport, Aquaculture, Reclamation, Mudpit</li>
+                  <li>Southeast Sulawesi Sanitary Landﬁll, Seaport</li>
 
-                    <li>29. Nort Maluku Power Plant, Sanitary Landﬁll</li>
+                  <li>South Sulawesi Seaport, Aquaculture, Reclamation, Mudpit</li>
 
-                    <li>30. Maluku Sanitary Landﬁll, WWTP</li>
+                  <li>Nort Maluku Power Plant, Sanitary Landﬁll</li>
 
-                    <li>31. West Papua Sanitary Landﬁll</li>
+                  <li>Maluku Sanitary Landﬁll, WWTP</li>
 
-                    <li>32. Papua Mining, Sanitary Landﬁll</li>
+                  <li>West Papua Sanitary Landﬁll</li>
+
+                  <li>Papua Mining, Sanitary Landﬁll</li>
                   </ol>
                 </div>
 
